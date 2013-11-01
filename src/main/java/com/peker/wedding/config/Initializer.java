7f2e69f5f170880731d6c98091084f95996ed413
Vitaly Peker <pekerv@gmail.com>
2013-11-01 18:41:44 -0400

    Ported xml config to java config and added spring data dependencies.
    Using URLBasedViewResolver unless we need to move to a tiles. I don't
    think we will.

---
+++ src/main/java/com/peker/wedding/config/Initializer.java
@@ -0,0 +1,29 @@
+package com.peker.wedding.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class Initializer implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext)
+			throws ServletException {
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();  
+        ctx.register(WebAppConfig.class);  
+          
+        ctx.setServletContext(servletContext);    
+        
+        Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));  
+        servlet.addMapping("/");  
+        servlet.setLoadOnStartup(1);
+        
+        servletContext.addListener(new ContextLoaderListener(ctx));
+	}
+
+}
---
+++ src/main/java/com/peker/wedding/config/WebAppConfig.java
@@ -0,0 +1,24 @@
+package com.peker.wedding.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+@ComponentScan("com.peker.wedding")
+@EnableWebMvc
+public class WebAppConfig {
+
+	@Bean  
+    public UrlBasedViewResolver setupViewResolver() {  
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();  
+        resolver.setPrefix("/WEB-INF/views/");  
+        resolver.setSuffix(".jsp");  
+        resolver.setViewClass(JstlView.class);  
+        return resolver;  
+    }
+	
+}
