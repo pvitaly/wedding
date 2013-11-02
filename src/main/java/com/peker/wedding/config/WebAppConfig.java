b2539b8575a8bd0ef3378af2fe41f835cebd5b64
Vitaly Peker <pekerv@gmail.com>
2013-11-02 16:22:35 -0400

    Added styling and script rsourcs

--- src/main/java/com/peker/wedding/config/WebAppConfig.java
+++ src/main/java/com/peker/wedding/config/WebAppConfig.java
@@ -4,21 +4,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @Configuration
 @ComponentScan("com.peker.wedding")
 @EnableWebMvc
-public class WebAppConfig {
+public class WebAppConfig extends WebMvcConfigurerAdapter {
 
-	@Bean  
-    public UrlBasedViewResolver setupViewResolver() {  
-        UrlBasedViewResolver resolver = new UrlBasedViewResolver();  
-        resolver.setPrefix("/WEB-INF/views/");  
-        resolver.setSuffix(".jsp");  
-        resolver.setViewClass(JstlView.class);  
-        return resolver;  
-    }
-	
+	@Bean
+	public UrlBasedViewResolver setupViewResolver() {
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/images/*").addResourceLocations(
+				"/images/");
+		registry.addResourceHandler("/img/*").addResourceLocations("/img/");
+		registry.addResourceHandler("/css/*").addResourceLocations("/css/");
+		registry.addResourceHandler("/js/*").addResourceLocations("/js/");
+	}
+
 }
