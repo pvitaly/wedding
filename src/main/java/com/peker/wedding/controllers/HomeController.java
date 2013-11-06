0c947018cb7c2bb3bc76fbacdb6dca0589fda989
Vitaly Peker <pekerv@gmail.com>
2013-11-06 08:08:53 -0500

    Added controller methods for the RSVP functionality

--- src/main/java/com/peker/wedding/controllers/HomeController.java
+++ src/main/java/com/peker/wedding/controllers/HomeController.java
@@ -1,39 +1,49 @@
 package com.peker.wedding.controllers;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.peker.wedding.entities.Guest;
+import com.peker.wedding.services.IGuestService;
+import com.peker.wedding.util.GuestStatusEnum;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+
+	@Autowired
+	private IGuestService	guestService;
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+	public String home() {
 		return "home";
 	}
-	
+
+	@RequestMapping(value = "/rsvp", method = RequestMethod.PUT)
+	public RedirectView rsvp(String uniqueId, String response, Model model) {
+		RedirectView view = null;
+
+		Guest guest = this.guestService.findByUniqueId(uniqueId);
+
+		if (response.equalsIgnoreCase(GuestStatusEnum.DECLINED.toString())) {
+			this.guestService.regretGuest(guest);
+			view = new RedirectView("/wedding");
+		} else if (response.equalsIgnoreCase(GuestStatusEnum.ACCEPTED.toString())) {
+			view = new RedirectView("/wedding/rsvp");
+		}
+
+		return view;
+	}
+
+	@RequestMapping(value = "/rsvp", method = RequestMethod.GET)
+	public String rsvp() {
+		return "rsvp";
+	}
+
 }
