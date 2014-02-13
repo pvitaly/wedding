d06a6c527b1eb6e6cdc1d0d7fe62013d3f4b1ac9
pvitaly <pekerv@gmail.com>
2014-02-13 16:58:41 -0500

    Deleted methods returning views. Created methods for accepting and regret.

--- src/main/java/com/peker/wedding/controllers/HomeController.java
+++ src/main/java/com/peker/wedding/controllers/HomeController.java
@@ -2,16 +2,14 @@ package com.peker.wedding.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.view.RedirectView;
 
 import com.google.gson.Gson;
 import com.peker.wedding.entities.Guest;
 import com.peker.wedding.services.IGuestService;
-import com.peker.wedding.util.GuestStatusEnum;
 
 /**
  * Handles requests for the application home page.
@@ -19,109 +17,92 @@ import com.peker.wedding.util.GuestStatusEnum;
 @Controller
 public class HomeController {
 
-	private static final String	REDIRECT	= "redirect:/";
-	@Autowired
-	private IGuestService		guestService;
+    private static final String EMPTY_JSON = "{}";
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home() {
-		return "home";
-	}
+    @Autowired
+    private IGuestService guestService;
 
-	@RequestMapping(value = "404", method = RequestMethod.GET)
-	public String pageNotFoundView() {
-		return "404";
-	}
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+        return EMPTY_JSON;
+    }
 
-	@RequestMapping(value = "/rsvp", method = RequestMethod.PUT)
-	public RedirectView rsvp(String uniqueId, String response, Model model) {
-		RedirectView view = null;
+    @RequestMapping(value = "/accept/{uniqueId}", method = RequestMethod.PUT, produces = "application/json")
+    public @ResponseBody
+    String rsvpAccept(@PathVariable String uniqueId) {
+        if (uniqueId != null && uniqueId.length() > 0) {
+            Guest guest = this.guestService.findByUniqueId(uniqueId);
 
-		if (uniqueId != null) {
-			Guest guest = this.guestService.findByUniqueId(uniqueId);
+            if (guest != null && guest.getId() != null) {
+                Guest acceptGuest = this.guestService.acceptGuest(guest);
+                return acceptGuest.toString();
+            }
+        }
+        return EMPTY_JSON;
+    }
 
-			if (guest == null || guest.getId() == null) {
-				view = new RedirectView("/");
-				return view;
-			}
+    @RequestMapping(value = "/decline/{uniqueId}", method = RequestMethod.PUT, produces = "application/json")
+    public @ResponseBody
+    String rsvpRegret(@PathVariable String uniqueId) {
+        if (uniqueId != null && uniqueId.length() > 0) {
+            Guest guest = this.guestService.findByUniqueId(uniqueId);
 
-			if (response.equalsIgnoreCase(GuestStatusEnum.DECLINED.toString())) {
-				this.guestService.regretGuest(guest);
-				view = new RedirectView("/");
-			} else if (response.equalsIgnoreCase(GuestStatusEnum.ACCEPTED.toString())) {
-				this.guestService.acceptGuest(guest);
-				view = new RedirectView("/rsvp?uniqueId=" + uniqueId);
-			}
+            if (guest != null && guest.getId() != null) {
+                Guest acceptGuest = this.guestService.regretGuest(guest);
+                return acceptGuest.toString();
+            }
+        }
+        return EMPTY_JSON;
+    }
 
-			return view;
-		}
-		view = new RedirectView("/");
-		return view;
-	}
+    @RequestMapping(value = "rsvp/setPlusOne", method = RequestMethod.POST)
+    public @ResponseBody
+    String rsvpClearPlusOne(Integer id, Boolean plusOne) {
+        Guest guest = this.guestService.findOne(id);
+        guest.setPlusOne(plusOne);
 
-	@RequestMapping(value = "/rsvp", method = RequestMethod.GET)
-	public String rsvp(String uniqueId, Model model) {
-		if (uniqueId != null) {
-			Guest guest = this.guestService.findByUniqueId(uniqueId);
+        this.guestService.save(guest);
 
-			if (guest == null || guest.getId() == null) {
-				return REDIRECT;
-			}
+        Gson gson = new Gson();
+        String json = gson.toJson(guest);
 
-			model.addAttribute("guest", guest);
-			return "rsvp";
-		}
-		return REDIRECT;
-	}
+        return json;
+    }
 
-	@RequestMapping(value = "rsvp/setPlusOne", method = RequestMethod.POST)
-	public @ResponseBody
-	String rsvpClearPlusOne(Integer id, Boolean plusOne) {
-		Guest guest = this.guestService.findOne(id);
-		guest.setPlusOne(plusOne);
+    @RequestMapping(value = "rsvp/savePlusOneName", method = RequestMethod.POST)
+    public @ResponseBody
+    String addPlusOneFirstName(Integer id, String firstName, String lastName) {
+        Guest guest = this.guestService.findOne(id);
+        guest.setPlusOneFirstName(firstName);
+        guest.setPlusOneLastName(lastName);
 
-		this.guestService.save(guest);
+        this.guestService.save(guest);
 
-		Gson gson = new Gson();
-		String json = gson.toJson(guest);
+        String name = guest.getPlusOneFirstName() + " " + guest.getPlusOneLastName();
 
-		return json;
-	}
+        return name;
+    }
 
-	@RequestMapping(value = "rsvp/savePlusOneName", method = RequestMethod.POST)
-	public @ResponseBody
-	String addPlusOneFirstName(Integer id, String firstName, String lastName) {
-		Guest guest = this.guestService.findOne(id);
-		guest.setPlusOneFirstName(firstName);
-		guest.setPlusOneLastName(lastName);
+    @RequestMapping(value = "rsvp/setKids", method = RequestMethod.POST)
+    public @ResponseBody
+    String rsvpSetKids(Integer id, Boolean bringingKids) {
+        Guest guest = this.guestService.findOne(id);
+        guest.setBringingKids(bringingKids);
 
-		this.guestService.save(guest);
+        this.guestService.save(guest);
 
-		String name = guest.getPlusOneFirstName() + " " + guest.getPlusOneLastName();
+        return null;
+    }
 
-		return name;
-	}
+    @RequestMapping(value = "rsvp/saveKidsNames", method = RequestMethod.POST)
+    public @ResponseBody
+    String saveKidsNames(Integer id, String names) {
+        Guest guest = this.guestService.findOne(id);
+        guest.setKidNames(names);
 
-	@RequestMapping(value = "rsvp/setKids", method = RequestMethod.POST)
-	public @ResponseBody
-	String rsvpSetKids(Integer id, Boolean bringingKids) {
-		Guest guest = this.guestService.findOne(id);
-		guest.setBringingKids(bringingKids);
+        this.guestService.save(guest);
 
-		this.guestService.save(guest);
-
-		return null;
-	}
-
-	@RequestMapping(value = "rsvp/saveKidsNames", method = RequestMethod.POST)
-	public @ResponseBody
-	String saveKidsNames(Integer id, String names) {
-		Guest guest = this.guestService.findOne(id);
-		guest.setKidNames(names);
-
-		this.guestService.save(guest);
-
-		return null;
-	}
+        return null;
+    }
 
 }
