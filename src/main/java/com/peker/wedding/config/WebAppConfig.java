db7fb07bd39dd19c4077aa21ff2b4fa60cee6de4
pvitaly <pekerv@gmail.com>
2014-02-13 16:58:53 -0500

    Got rid of view resolver

--- src/main/java/com/peker/wedding/config/WebAppConfig.java
+++ src/main/java/com/peker/wedding/config/WebAppConfig.java
@@ -1,33 +1,13 @@
 package com.peker.wedding.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.JstlView;
-import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @Configuration
 @ComponentScan("com.peker.wedding")
 @EnableWebMvc
 public class WebAppConfig extends WebMvcConfigurerAdapter {
 
-	@Bean
-	public UrlBasedViewResolver setupViewResolver() {
-		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
-		resolver.setPrefix("/WEB-INF/views/");
-		resolver.setSuffix(".jsp");
-		resolver.setViewClass(JstlView.class);
-		return resolver;
-	}
-
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/images/*").addResourceLocations("/images/");
-		registry.addResourceHandler("/img/*").addResourceLocations("/img/");
-		registry.addResourceHandler("/css/**").addResourceLocations("/css/");
-		registry.addResourceHandler("/js/**").addResourceLocations("/js/");
-	}
 }
